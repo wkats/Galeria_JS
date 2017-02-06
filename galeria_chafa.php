4f1babedf0c322d446b3a78f4c8541b267a9fc8c
wkats <mtzolrene@gmail.com>
2017-02-06 11:50:11 -0600

    Empezando

---
+++ galeria_chafa.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>:.Galería.:</title>
+	<script
+	src="https://code.jquery.com/jquery-3.1.1.min.js"
+	integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+	crossorigin="anonymous">			  	
+	</script>
+	
+	<link rel="stylesheet" type="text/css" href="./css/galeria.css">
+</head>
+<body>
+
+<div id="dSlider">
+	<div class="dImgShow">
+		<img src="img/00.jpg" alt="None">
+	</div>
+	
+	<div class="dPreview">
+		<ul>
+			<li><img src="img/00.jpg" alt=""></li>
+			<li><img src="img/01.jpg" alt=""></li>
+			<li><img src="img/02.jpg" alt=""></li>
+			<li><img src="img/03.jpg" alt=""></li>
+			<li><img src="img/04.jpg" alt=""></li>
+			<li><img src="img/05.png" alt=""></li>
+			<li><img src="img/06.jpg" alt=""></li>
+			<li><img src="img/07.jpg" alt=""></li>
+			<li><img src="img/08.png" alt=""></li>
+			<li><img src="img/09.jpg" alt=""></li>
+		</ul>
+	</div>
+</div>
+</body>
+</html>
