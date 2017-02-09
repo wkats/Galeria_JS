1af8d1925479a4b7e87176e20fc8928ebf8d4ec1
wkats <mtzolrene@gmail.com>
2017-02-09 12:48:10 -0600

    Fix CSS para Chrome|Selector de imágenes por click

--- galeria_chafa.php
+++ galeria_chafa.php
@@ -15,23 +15,39 @@
 
 <div id="dSlider">
 	<div class="dImgShow">
-		<img src="img/00.jpg" alt="None">
+		<img src="img/00.png" alt="None">
 	</div>
 	
 	<div class="dPreview">
 		<ul>
-			<li><img src="img/00.jpg" alt=""></li>
+			<li><img src="img/00.png" alt=""></li>
 			<li><img src="img/01.jpg" alt=""></li>
-			<li><img src="img/02.jpg" alt=""></li>
+			<li><img src="img/02.gif" alt=""></li>
 			<li><img src="img/03.jpg" alt=""></li>
-			<li><img src="img/04.jpg" alt=""></li>
-			<li><img src="img/05.png" alt=""></li>
-			<li><img src="img/06.jpg" alt=""></li>
-			<li><img src="img/07.jpg" alt=""></li>
-			<li><img src="img/08.png" alt=""></li>
-			<li><img src="img/09.jpg" alt=""></li>
+			<li><img src="img/04.gif" alt=""></li>
+			<li><img src="img/05.jpg" alt=""></li>
+			<li><img src="img/06.png" alt=""></li>			
 		</ul>
 	</div>
 </div>
+<script type="text/javascript">
+	var imgShow;
+	var prevImgs;
+	$(document).ready(init());
+	function init(){
+		imgShow = $("#dSlider .dImgShow img");		
+		imgShow.attr('src', 'img/02.jpg');	
+		prevImgs = $("#dSlider .dPreview ul li img")
+		prevImgs.click(function(event) {
+			prevClick($(this));
+		});
+	}
+	function prevClick(imgObject){
+		//console.log(imgObject);		
+		var str = imgObject.attr('src');
+		console.log(imgObject.attr('src'));
+		imgShow.attr("src", imgObject.attr("src"));
+	}
+</script>
 </body>
 </html>
