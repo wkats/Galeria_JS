e298e3fdaca0b4a5fd61b66b98da68a3278ed981
wkats <mtzolrene@gmail.com>
2017-02-09 16:25:14 -0600

    Añadida variable debug para contorlar el código temporal

--- js/galery_functions.js
+++ js/galery_functions.js
@@ -1,3 +1,5 @@
+var debug = true;
+var dPreview;
 var imgShow;
 var prevImgs;
 var temporizador;
@@ -5,23 +7,34 @@ var incremento = 50;
 $(document).ready(init());
 	
 function init(){
+	if(debug){
+		console.log("Debugging... Hello World");
+	}
+	dPreview = $("#dSlider .dPreview");	
 	imgShow = $("#dSlider .dImgShow img");		
-	imgShow.attr('src', 'img/02.gif');	
-	prevImgs = $("#dSlider .dPreview ul li img")
+	imgShow.attr('src', 'img/00.png');	
+
+	prevImgs = dPreview.find('ul li img')
 	prevImgs.click(function(event) {
 		prevClick($(this));
 	});
+
+/*
 	var coso = $("#dSlider .dPreview ul");	
-	temporizador = setInterval(function(){movimiento(coso);},1000);
+	console.log(coso);
+	//temporizador = setInterval(function(){movimiento(coso);},1000);
 	//temporizador = setInterval(movimiento,1500);
+	*/
 }
 
 function prevClick(imgObject){
-	console.log(imgObject);			
-	var str = imgObject.attr('src');
-	console.log(imgObject.attr('src'));
+	if(debug){
+		console.log(imgObject);				
+		console.log(imgObject.attr('src'));		
+	}
 	imgShow.attr("src", imgObject.attr("src"));
 }
 
-function movimiento(_ul){	
+function movimiento(_ul){			
 	_ul.animate({"margin-left":"+=50"},1000);	
+}
